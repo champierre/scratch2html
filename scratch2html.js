2dc842663238d4b56538221a6354ed491315a541
Junya Ishihara <junya@champierre.com>
2016-09-30 02:19:10 +0900

    Change domain.

--- scratch2html.js
+++ scratch2html.js
@@ -1,6 +1,8 @@
 (function(ext) {
     var html;
-    
+    var host = "http://www.scratch2html.com";
+//    var host = "http://localhost:3000";
+
     ext._shutdown = function() {};
 
     ext._getStatus = function() {
@@ -54,6 +56,46 @@
         });
     };
 
+    ext.table = function() {
+        tag = '<table border="1">';
+        html += tag;
+        $.post('http://localhost:5678/add', {tag: tag}, function() {
+            console.log('add tag:' + tag);
+        });
+    };
+
+    ext.tr = function() {
+        tag = '<tr>';
+        html += tag;
+        $.post('http://localhost:5678/add', {tag: tag}, function() {
+            console.log('add tag:' + tag);
+        });
+    };
+
+    ext.td = function(str) {
+        tag = '<td>' + str + '</td>';
+        html += tag;
+        $.post('http://localhost:5678/add', {tag: tag}, function() {
+            console.log('add tag:' + tag);
+        });
+    };
+
+    ext.tr_end = function() {
+        tag = '</tr>';
+        html += tag;
+        $.post('http://localhost:5678/add', {tag: tag}, function() {
+            console.log('add tag:' + tag);
+        });
+    };
+
+    ext.table_end = function() {
+        tag = '</table>';
+        html += tag;
+        $.post('http://localhost:5678/add', {tag: tag}, function() {
+            console.log('add tag:' + tag);
+        });
+    };
+
     ext.body_end = function() {
         tag = '</body>';
         html += tag;
@@ -69,9 +111,9 @@
             console.log('add tag: </html>');
         });
     };
-    
+
     ext.publish = function(str) {
-        $.post('https://scratch2html.herokuapp.com/sites.json', {'site[slug]': str, 'site[html]': html}, function() {
+        $.post(host + '/sites.json', {'site[slug]': str, 'site[html]': html}, function() {
             console.log('post:' + html);
         });
     };
@@ -83,10 +125,15 @@
             [' ', '<h1> %s </h1>', 'h1', 'h1'],
             [' ', '<p> %s </p>', 'p', 'p'],
             [' ', '<br />', 'br'],
+            [' ', '<table border="1">', 'table'],
+            [' ', '<tr>', 'tr'],
+            [' ', '<td> %s </td>', 'td', 'td'],
             [' ', '<img src=" %s ">', 'img', 'https://wiki.scratch.mit.edu/w/images/Cat.png'],
+            [' ', '</tr>', 'tr_end'],
+            [' ', '</table>', 'table_end'],
             [' ', '</body>', 'body_end'],
             [' ', '</html>', 'html_end'],
-            [' ', 'Publish to https://scratch2html.herokuapp.com/ %s', 'publish', '']
+            [' ', 'Publish to https://www.scratch2html.com/ %s', 'publish', '']
         ]
     };
 
